9f9818f3ebecacbcc0a1ae17d364bb07773f4b16
Maral <maral.nalbandian@student.uts.edu.au>
2019-08-30 11:32:26 +1000

    Update user interface

--- Front-end/src/components/Discussion.js
+++ Front-end/src/components/Discussion.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Nav from "./Nav";
 
 function Discussion() {
   return (
     <div>
+      <Nav />
       <h1>Discussion Page</h1>
     </div>
   );
--- Front-end/src/components/Leaderboard.js
+++ Front-end/src/components/Leaderboard.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Nav from "./Nav";
 
 function Leaderboard() {
   return (
     <div>
+      <Nav />
       <h1>Leaderboard Page</h1>
     </div>
   );
